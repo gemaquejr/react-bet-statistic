9130f387410270b064fe1e6790853674cfdfd23e
Francisco Gemaque <gemaquejr@hotmail.com>
2025-02-05 15:26:50 -0300

    Cria servidor
    Signed-off-by: Francisco Gemaque <gemaquejr@hotmail.com>

---
+++ backend/src/server.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
