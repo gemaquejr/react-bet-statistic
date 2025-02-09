19f303570eeaca922f8a18bc4dc479301d9a8edf
Francisco Gemaque <gemaquejr@hotmail.com>
2025-02-09 14:46:13 -0300

    Fix: server.ts
    Signed-off-by: Francisco Gemaque <gemaquejr@hotmail.com>

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,15 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import userRouter from './routes/userRoute';
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+app.use('/', userRouter);
 
 const PORT = process.env.PORT;
 
