0b4b329da320953b301c3c0cc157404717331194
Francisco Gemaque <gemaquejr@hotmail.com>
2025-02-09 14:45:39 -0300

    Adiciona userRouter.ts
    Signed-off-by: Francisco Gemaque <gemaquejr@hotmail.com>

---
+++ backend/src/routes/userRoute.ts
@@ -0,0 +1,16 @@
+import { Router, Request, Response } from 'express';
+import UserController from '../controller/userController';
+import loginValidation from '../middlewares/loginValidation';
+
+const router = Router();
+const userController = new UserController();
+
+router.post('/login', loginValidation, (req: Request, res: Response) => {
+  userController.loginController(req, res);
+});
+
+router.get('/validate', (req: Request, res: Response ) => {
+  userController.getLogin(req, res);
+});
+
+export default router;
